c02a3d527528766aa2c2582e5bd2f8ff6f27eace
Prayag019 <rajkar3471@gmail.com>
2026-01-17 14:57:54 +0545

    initial table created

--- root/myproject/api/models.py
+++ root/myproject/api/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+
+
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+
+    role=models.CharField(max_length=20,choices=[('teacher','Teacher'),('student','Student')],default='studnet')
+    full_name=models.CharField(max_length=100)
+    permanant_address=models.CharField(max_length=100)
+    current_address=models.CharField(max_length=100)
+    age=models.IntegerField()
+    verified=models.BooleanField(default=False)
+
+
+
+class Qualification(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    highschool_status = models.CharField( max_length=10, choices=[('completed', 'Completed'), ('pending', 'Pending')], default='pending')
+    highschool_institution = models.CharField(max_length=255, blank=True, null=True)
+    bachelor_status = models.CharField( max_length=10, choices=[('completed', 'Completed'), ('pending', 'Pending')], default='pending' )
+    bachelor_institution = models.CharField(max_length=255, blank=True, null=True)
+    master_status = models.CharField( max_length=10, choices=[('completed', 'Completed'), ('pending', 'Pending')], default='pending' )
+    master_institution = models.CharField(max_length=255, blank=True, null=True)
+
+
+
+class Vacancy(models.Model):
+    vacancy_type=models.CharField(max_length=50,choices=[('tution_teacher','Tution_Teacher'),('school_teacher','School_teacher')],default='tution_teacher')
+    status=models.CharField(max_length=25,default='open',choices=[('open','Open'),('closed','Closed')])
+    grade=models.IntegerField()
+    salary=models.IntegerField()
+    location=models.CharField(max_length=100)
+    description=models.TextField()
+
+    
+
+
+    
+
+
+
+
+
+
